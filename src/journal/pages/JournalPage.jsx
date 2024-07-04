bf803b84e3677b309fe3ca740d1ca8e9f4f68adb
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-07-04 00:23:57 -0300

    noteview done!

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../view";
+import { NoteView, NothingSelectedView } from "../view";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -10,6 +12,21 @@ export const JournalPage = () => {
         nisi voluptas. Veritatis commodi quae sunt velit expedita, eos quod!
       </Typography> */}
       <NothingSelectedView></NothingSelectedView>
+      {/* <NoteView></NoteView> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
